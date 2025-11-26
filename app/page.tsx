2a881ca5fc933dd3b8abcd952003bc453122fe13
parkerCYH <fbi0258zzz@gmail.com>
2025-11-26 19:05:00 +0800

    Remove MUI dependencies and replace with native HTML elements

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,7 @@
-import { Button } from "@mui/material";
-
 export default function Home() {
   return (
     <div>
-      <Button variant="contained">Hello World</Button>
+      <button>Hello World</button>
     </div>
   );
 }
